d236affc6312b190fcf4cbff134c8d5aa87357a8
Mohasin Hossain <mohasinhossainrajib2@gmail.com>
2024-06-12 17:28:29 +0600

    Initial Setup

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,28 @@
+import { createBrowserRouter } from "react-router-dom";
+import Root from "../layout/Root/Root";
+import Home from "../pages/Home/Home";
+import AddCoffee from "../pages/AddCoffee/AddCoffee";
+import UpdateCoffee from "../pages/UpdateCoffee/UpdateCoffee";
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/addCoffee",
+        element: <AddCoffee></AddCoffee>,
+      },
+      {
+        path: "/updateCoffee",
+        element: <UpdateCoffee></UpdateCoffee>,
+      },
+    ],
+  },
+]);
+
+export default routes;
